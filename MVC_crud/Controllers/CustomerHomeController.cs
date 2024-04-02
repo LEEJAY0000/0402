50d99c7f528904339273a7db162084174d4bfb52
JAY <zzz022679@gmail.com>
2024-04-02 19:04:57 +0800

    0402

---
+++ MVC_crud/Controllers/CustomerHomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_crud.Data;
+
+namespace MVC_crud.Controllers
+{
+    public class CustomerHomeController : Controller
+    {
+        private readonly MVCDBContext mvcDBContext;
+
+        public CustomerHomeController(MVCDBContext mvcDBContext)
+        {
+            this.mvcDBContext = mvcDBContext;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> ListCust()
+        {
+            var customers = await mvcDBContext.Customers.ToListAsync();
+            return View(customers);
+        }
+    }
+}
---
+++ MVC_crud/Data/MVCDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_crud.Models.Entities;
+
+namespace MVC_crud.Data
+{
+    public class MVCDBContext : DbContext
+    {
+        public MVCDBContext(DbContextOptions<MVCDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customers> Customers { get; set; }
+    }
+}
